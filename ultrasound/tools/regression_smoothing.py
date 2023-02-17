6f7cb286b81c88ae9759315be98bdef19a108156
David <david.abraham.1337@gmail.com>
2023-02-17 12:55:57 -0500

    Update smoothing algos

--- ultrasound/tools/regression_smoothing.py
+++ ultrasound/tools/regression_smoothing.py
@@ -42,10 +42,151 @@ def linear_regression(x, y):
     m, c = np.linalg.lstsq(A, y, rcond=None)[0]
     y_reg = m * x + c
     r2 = r_squared(y, y_reg)
-    return y_reg, r2
+    return y_reg, r2, m, c
 
 
-def auto_regression_smoothing(time, values, min_r2_score=0.9, max_regression_len=48, regression_weight=0.9):
+def auto_regression_smoothing(time, values, params, min_r2_score=0.9, max_regression_len=48, regression_weight=0.9):
+    """Live smoothing of a dataset using linear regression
+
+    Parameters
+    ----------
+    time : np.array or list
+        timestamps of each data point
+    values : np.array or list
+        values of each data points
+    min_r2_score : float, optional
+        minimum r squared metric to keep the smoothed data point, by default 0.8
+    max_regression_len : int, optional
+        maximum number of previous data points to use for the linear regression, by default 48
+    regression_weight : float, optional
+        weight of the regression result to use compared to the original data value, by default 0.9
+
+    Returns
+    -------
+    float
+        smoothed data value
+    """
+    assert len(values) >= max_regression_len, "There must be more or equal previous data than the maximum regression length."
+    assert len(values) == len(time), "There must be the same number of values and time index."
+
+    if params["delta_value"] > params["min_fill_value"]:
+        return values[-1]
+
+    values = np.array(values)
+    time = np.array(time, dtype=np.datetime64)
+
+    time = time.astype(np.float)
+    time = (time - np.min(time)) / (np.max(time) - np.min(time))
+
+    # Sort arrays according to time
+    sort_indices = time.argsort()
+    time = time[sort_indices]
+    values = values[sort_indices]
+
+    best_r2 = -100
+    best_time = []
+    best_values = []
+    best_values_reg = []
+    for i in range(len(values) - max_regression_len, len(values) - 5, 3):
+        x_slice = time[i:]
+        y_slice = values[i:]
+
+        y_reg, r2, _, _ = linear_regression(x_slice, y_slice)
+        if r2 > best_r2:
+            best_r2 = r2
+            best_time = x_slice
+            best_values = y_slice
+            best_values_reg = y_reg
+
+    smoothed_value = best_values[-1]
+    if best_r2 > min_r2_score:
+        smoothed_value = best_values_reg[-1] * regression_weight + best_values[-1] * (1 - regression_weight)
+    smoothed_value = max(0, smoothed_value)
+
+    return smoothed_value
+
+
+def exp_filter(prev, current, params):
+    # a between 0.8 and 0.99
+    # params = {"tau": 0.8, "timestep": 1}
+    if params["delta_value"] > params["min_fill_value"]:
+        return current, params
+    a = np.exp(-params["timestep"] / params["tau"])
+    filtered = prev + (1 - a) * (current - prev)
+    return filtered, params
+
+
+def nonlin_exp_filter(prev, current, params):
+    # R between 3 and 5
+    # params = {"R": 3.5}
+    if params["delta_value"] > 3:
+        return current, params
+    delta_xn = current - prev
+    f = min(1, abs(delta_xn / params["R"]))
+    filtered = prev + f * delta_xn
+    return filtered, params
+
+
+def generic_iir_filter(data, filter_func, params):
+    data = np.array(data)
+    filtered_data = np.zeros_like(data)
+    filtered_data[0] = data[0]
+
+    params["max_value"] = np.max(data)
+    for i in range(1, len(data)):
+        params["delta_value"] = data[i] - data[i - 1]
+        filtered_data[i], params = filter_func(filtered_data[i - 1], data[i], params)
+
+    return filtered_data
+
+
+def spike_filter(prev_filtered_value, current_value, params):
+    # params = {"maximum_change_perc": 1, "number_of_changes": 2, "count": 0}
+
+    # if params["delta_value"] > 3:
+    #    params["count"] = 0
+    #    return current_value, params
+
+    current_filtered_value = 0
+
+    change_perc = (abs(current_value - prev_filtered_value) / params["bin_max"]) * 100
+
+    if change_perc > params["maximum_change_perc"] and params["count"] < params["number_of_changes"]:
+        params["count"] += 1
+        current_filtered_value = prev_filtered_value
+    else:
+        params["count"] = 0
+        current_filtered_value = current_value
+
+    return current_filtered_value, params
+
+
+def smoothing(times, values, prev_smoothed_value, min_r2_score=0.9, max_regression_len=48, regression_weight=0.9):
+    # TODO make everything a param of the function
+    min_fill_value = 3
+
+    assert len(values) == len(times), "There must be the same number of values and time index."
+    assert len(values) >= 1, "There must be at least 1 value."
+    if len(values) == 1:
+        return values[0]
+
+    spike_filtered = generic_iir_filter(values, spike_filter, {"maximum_change_perc": 5, "number_of_changes": 2, "count": 0, "bin_max": 25})
+    delta_value = spike_filtered[len(spike_filtered) - 1] - spike_filtered[len(spike_filtered) - 2]
+    if len(values) < max_regression_len:
+        smoothed_value, _ = exp_filter(prev_smoothed_value, spike_filtered[-1], {
+            "tau": 2, "timestep": 1, "min_fill_value": min_fill_value, "delta_value": delta_value})
+        reg_smoothed_value = 0
+    else:
+        reg_smoothed_value = auto_regression_smoothing(
+            times, spike_filtered, {"min_fill_value": min_fill_value, "delta_value": delta_value},
+            min_r2_score, max_regression_len, regression_weight)
+        smoothed_value, _ = exp_filter(prev_smoothed_value, reg_smoothed_value, {
+            "tau": 2, "timestep": 1, "min_fill_value": min_fill_value, "delta_value": delta_value})
+
+    return smoothed_value
+
+
+def new_auto_regression_smoothing(time, values, min_r2_score=0.9, max_regression_len=48, regression_weight=0.9):
     """Live smoothing of a dataset using linear regression
 
     Parameters
@@ -84,11 +225,11 @@ def auto_regression_smoothing(time, values, min_r2_score=0.9, max_regression_len
     best_time = []
     best_values = []
     best_values_reg = []
-    for i in range(len(values) - max_regression_len, len(values) - 5, 3):
+    for i in range(len(values) - max_regression_len, len(values) - 5, 1):
         x_slice = time[i:]
         y_slice = values[i:]
 
-        y_reg, r2 = linear_regression(x_slice, y_slice)
+        y_reg, r2, _, _ = linear_regression(x_slice, y_slice)
         if r2 > best_r2:
             best_r2 = r2
             best_time = x_slice
--- ultrasound/tools/tests/test_smoothing.py
+++ ultrasound/tools/tests/test_smoothing.py
@@ -6,19 +6,79 @@ import json
 import tools.regression_smoothing as rs
 
 
-def exp_filter(prev_filtered_value, current_value, a=0.9):
+def exp_filter_old(prev_filtered_value, current_value, a=0.9):
     # a between 0.8 and 0.99
     return prev_filtered_value + (1 - a) * (current_value - prev_filtered_value)
 
 
-def nonlin_exp_filter(prev_filtered_value, current_value, R=3.5):
+def exp_filter(prev, current, params):
+    # a between 0.8 and 0.99
+    # params = {"tau": 0.8, "timestep": 1}
+    if params["delta_value"] > 3:
+        return current, params
+    a = np.exp(-params["timestep"] / params["tau"])
+    filtered = prev + (1 - a) * (current - prev)
+    return filtered, params
+
+
+def nonlin_exp_filter(prev, current, params):
+    # R between 3 and 5
+    # params = {"R": 3.5}
+    if params["delta_value"] > 3:
+        return current, params
+    delta_xn = current - prev
+    f = min(1, abs(delta_xn / params["R"]))
+    filtered = prev + f * delta_xn
+    return filtered, params
+
+
+def generic_iir_filter(data, filter_func, params):
+    data = np.array(data)
+    filtered_data = np.zeros_like(data)
+    filtered_data[0] = data[0]
+    filtered_data_2 = np.zeros_like(data)
+    filtered_data_2[0] = data[0]
+
+    params["max_value"] = np.max(data)
+    for i in range(1, len(data)):
+        # if data[i] - data[i - 1] > 2:
+        #    filtered_data[i] = data[i]
+        # else:
+        params["delta_value"] = data[i] - data[i - 1]
+        filtered_data[i], params = filter_func(filtered_data[i - 1], data[i], params)
+
+    return filtered_data
+
+
+def nonlin_exp_filter_old(prev_filtered_value, current_value, R=3.5):
     # R between 3 and 5
     delta_xn = current_value - prev_filtered_value
     f = min(1, abs(delta_xn / R))
     return prev_filtered_value + f * delta_xn
 
 
-def spike_filter(prev_filtered_value, current_value, count, maximum_change, number_of_changes=2):
+def spike_filter(prev_filtered_value, current_value, params):
+    # params = {"maximum_change_perc": 1, "number_of_changes": 2, "count": 0}
+
+    # if params["delta_value"] > 3:
+    #    params["count"] = 0
+    #    return current_value, params
+
+    current_filtered_value = 0
+
+    change_perc = (abs(current_value - prev_filtered_value) / params["max_value"]) * 100
+
+    if change_perc > params["maximum_change_perc"] and params["count"] < params["number_of_changes"]:
+        params["count"] += 1
+        current_filtered_value = prev_filtered_value
+    else:
+        params["count"] = 0
+        current_filtered_value = current_value
+
+    return current_filtered_value, params
+
+
+def spike_filter_old(prev_filtered_value, current_value, count, maximum_change, number_of_changes=2):
     current_filtered_value = 0
 
     if abs(current_value - prev_filtered_value) > maximum_change and count < number_of_changes:
@@ -31,66 +91,86 @@ def spike_filter(prev_filtered_value, current_value, count, maximum_change, numb
     return current_filtered_value, count
 
 
-def savgol_filter():
+def savgol_filter(data, window_size, ):
     # TODO
     return
 
 
 if __name__ == "__main__":
-    data_path = "data/test/test2.csv"
+    data_path = "data/test/1486B.csv"
 
     data = pd.read_csv(data_path, converters={"AcquisitionTime": pd.to_datetime})
-    data = data[data["weightAlgo1_t"].notnull()]
-    data["weightAlgo1_t"] = data.apply(lambda x: float(x["weightAlgo1_t"].split(" ")[0]), axis=1)
-    data["smoothed"] = data.apply(lambda x: float(x["smoothed"].split(" ")[0]), axis=1)
-    data = data.resample("1H", on="AcquisitionTime").mean()
+    data = data[data["latestAlgo_w_t"].notnull()]
+    #data["weightAlgo1_t"] = data.apply(lambda x: float(x["weightAlgo1_t"].split(" ")[0]), axis=1)
+    #data["smoothed"] = data.apply(lambda x: float(x["smoothed"].split(" ")[0]), axis=1)
+
+    data = data.resample("1H", on="AcquisitionTime").mean(numeric_only=True)
     data["AcquisitionTime"] = data.index
-    data = data[data["weightAlgo1_t"].notnull()]
+    data = data[data["latestAlgo_w_t"].notnull()]
 
-    print(data.columns)
-    print(data)
-    print(data["AcquisitionTime"].diff().mean())
+    plt.plot(data["AcquisitionTime"], data["latestAlgo_w_t"], '.')
 
-    print(len(data))
+    spike_filtered = generic_iir_filter(data["latestAlgo_w_t"].values, spike_filter, params={
+        "maximum_change_perc": 5, "number_of_changes": 2, "count": 0})
+    #plt.plot(data["AcquisitionTime"], spike_filtered)
 
-    sv = np.zeros(len(data))
-    sv_exp = np.zeros(len(data))
-    sv_nonlin_exp = np.zeros(len(data))
-    sv_spike = np.zeros(len(data))
-    spike_fil_count = 0
-
-    for i in range(len(data)):
-        current_val = data["weightAlgo1_t"].values[i]
-        if i > 0:
-            prev_val = data["weightAlgo1_t"].values[i - 1]
-            exp_prev_filtered_val = sv_exp[i - 1]
-            exp_nonlin_prev_filtered_val = sv_nonlin_exp[i - 1]
-            spike_prev_filtered_val = sv_spike[i - 1]
-
-            if current_val - prev_val > 2:
-                exp_prev_filtered_val = current_val
-                exp_nonlin_prev_filtered_val = current_val
-        else:
-            exp_prev_filtered_val = data["weightAlgo1_t"].values[i]
-            exp_nonlin_prev_filtered_val = data["weightAlgo1_t"].values[i]
-            spike_prev_filtered_val = data["weightAlgo1_t"].values[i]
-
-        sv_exp[i] = exp_filter(exp_prev_filtered_val, current_val, a=0.8)
-        sv_nonlin_exp[i] = max(0, nonlin_exp_filter(exp_nonlin_prev_filtered_val, current_val, R=3.5))
-        sv_spike[i], spike_fil_count = spike_filter(spike_prev_filtered_val, current_val, spike_fil_count, 1, 2)
-
-        if i > 48:
-            sv[i] = rs.auto_regression_smoothing(data["AcquisitionTime"].values[i-48:i], data["weightAlgo1_t"].values[i-48:i])
-        else:
-            sv[i] = 0
-    plt.plot(data["AcquisitionTime"], data["weightAlgo1_t"], '.')
-    plt.plot(data["AcquisitionTime"], sv, label="regression")
-    plt.plot(data["AcquisitionTime"], sv_exp, label="exp filter")
-    plt.plot(data["AcquisitionTime"], sv_nonlin_exp, label="nonlin exp filter")
+    tau, R = 4, 3.5
+    exp_filtered = generic_iir_filter(spike_filtered, exp_filter, params={"tau": tau, "timestep": 1})
+    #plt.plot(data["AcquisitionTime"], exp_filtered, label=f"exp, tau={tau}")
+    nonlin_exp_filtered = generic_iir_filter(spike_filtered, nonlin_exp_filter, params={"R": R})
+    plt.plot(data["AcquisitionTime"], nonlin_exp_filtered, label=f"nonlin exp, R={R}")
     plt.legend(loc="best")
     plt.show()
 
-    plt.plot(data["AcquisitionTime"], data["weightAlgo1_t"], '.')
-    plt.plot(data["AcquisitionTime"], sv_spike, label="spike filter")
+    plt.plot(data["AcquisitionTime"], data["latestAlgo_w_t"], '.')
+    for tau in [3]:
+        exp_filtered = generic_iir_filter(spike_filtered, exp_filter, params={"tau": tau, "timestep": 1})
+        plt.plot(data["AcquisitionTime"], exp_filtered, label=f"exp, tau={tau}")
+
+    for R in [3, 5]:
+        nonlin_exp_filtered = generic_iir_filter(spike_filtered, nonlin_exp_filter, params={"R": R})
+        plt.plot(data["AcquisitionTime"], nonlin_exp_filtered, label=f"nonlin exp, R={R}")
+
     plt.legend(loc="best")
+    plt.show()
+
+    reg_smooth = np.zeros_like(spike_filtered)
+    reg_smooth[0] = spike_filtered[0]
+    for i in range(1, len(data["latestAlgo_w_t"].values)):
+        if i >= 48:
+            reg_smooth[i] = rs.auto_regression_smoothing(
+                data["AcquisitionTime"].values[i - 48: i + 1],
+                spike_filtered[i - 48: i + 1],
+                {"min_fill_value": 3, "delta_value": 0})
+        else:
+            reg_smooth[i] = spike_filtered[i]
+        #reg_smooth[i] = generic_iir_filter(reg_smooth[max(0, i - 48):i], exp_filter, params={"tau": 2, "timestep": 1})[-1]
+        #delta = float(spike_filtered[i] - spike_filtered[i-1])
+        #reg_smooth[i], params = exp_filter(reg_smooth[i-1], spike_filtered[i], {"tau": 2, "timestep": 1, "delta_value": delta})
+    filt = rs.generic_iir_filter(reg_smooth, rs.exp_filter, params={"tau": 2, "timestep": 1, "min_fill_value": 3})
+    plt.plot(data["latestAlgo_w_t"].values)
+    #plt.plot(data["AcquisitionTime"], reg_smooth, label="regression")
+    plt.plot(filt, label="cheated")
+
+    final_smoothed = np.zeros_like(data["latestAlgo_w_t"].values)
+    final_smoothed[0] = data["latestAlgo_w_t"].values[0]
+    for i in range(1, len(data["latestAlgo_w_t"].values)):
+        final_smoothed[i] = rs.smoothing(
+            data["AcquisitionTime"].values[max(0, i - 48): i],
+            data["latestAlgo_w_t"].values[max(0, i - 48): i],
+            final_smoothed[i - 1])
+    plt.plot(final_smoothed, label="final")
+    plt.legend(loc="best")
+    """
+    data2 = data["latestAlgo_w_t"].values[0:200]
+    time2 = data["AcquisitionTime"].values[0:200]
+    spike_filtered2 = generic_iir_filter(data2, spike_filter, params={
+        "maximum_change_perc": 5, "number_of_changes": 2, "count": 0})
+    reg_smooth = np.zeros_like(data2)
+    for i in range(48, len(data2)):
+        reg_smooth[i] = rs.auto_regression_smoothing(time2[i - 48:i], spike_filtered2[i - 48:i])
+        reg_smooth[i] = generic_iir_filter(reg_smooth[i - 48:i], exp_filter, params={"tau": 2, "timestep": 1})[-1]
+    #filt = generic_iir_filter(reg_smooth, exp_filter, params={"tau": 2, "timestep": 1})
+    plt.plot(time2, filt, "x")
+    """
     plt.show()
