0769b0ec520f04004e87f6a76f2d821c51ccd11f
David <david.abraham.1337@gmail.com>
2023-03-10 13:15:57 -0500

    Remove numpy references

--- ultrasound/tools/auto_gain.py
+++ ultrasound/tools/auto_gain.py
@@ -1,7 +1,136 @@
-import numpy as np
 
 
-def auto_gain_detection_v2(data, data_range=(0, 255), bang_end=2500):
+def find_next_minimum(data, start_index):
+    """Find the next local minimum in a raw ultrasound signal
+
+    Parameters
+    ----------
+    data : list or np.ndarray
+        raw ultrasound signal
+    start_index : int
+        index to start the search for the next minimum in data
+
+    Returns
+    -------
+    (int, int)
+        (index of the minimum, value of the minimum)
+    """
+    current_min_value = data[start_index]
+    current_min_index = start_index
+    end_of_min_plateau = 0
+
+    # Iterate on the signal until the signal stops decreasing
+    for i in range(start_index, len(data)):
+        val = data[i]
+        if val < current_min_value:
+            current_min_value = val
+            current_min_index = i
+        elif val > current_min_value:
+            end_of_min_plateau = i
+            break
+
+    # Find the "middle point" of the minimum plateau
+    current_min_index = (end_of_min_plateau + current_min_index) // 2
+    return current_min_index, current_min_value
+
+
+def find_next_minimum_over_threshold(data, start_index, threshold, max_index):
+    """Finds the next minimum in a dataset that is below a certain threshold
+
+    Parameters
+    ----------
+    data : list or np.ndarray
+        raw ultrasound signal
+    start_index : int
+        index to start the search for the next minimum in data
+    threshold : int
+        the minimum needs to be below this threshold
+    max_index : int
+        the max value for the minimum index
+
+    Returns
+    -------
+    int
+        index of the minimum
+    """
+    if start_index <= 0:
+        raise "Start index must be > 0."
+
+    valid = False
+    current_index = start_index
+    # Continue to find the next minimum until the value of the minimum
+    # is below a threshold
+    while not valid:
+        diff = data[current_index - 1] - data[current_index]
+        if diff > 0:
+            min_index, min_value = find_next_minimum(data, current_index)
+            current_index = min_index
+        else:
+            current_index += 1
+            continue
+
+        if min_value < threshold:
+            valid = True
+
+        current_index += 1
+        if current_index > max_index:
+            min_index = max_index
+            break
+
+    return min_index
+
+
+def detect_main_bang_end(data, pulse_count, max_bang_len=3000) -> int:
+    """Automatically detect the end of the main bang in a raw ultrasound reading
+
+    Parameters
+    ----------
+    data : list or numpy.ndarray
+        raw data of the ultrasound
+    max_mainbang_index : int, optional
+        max possible length of the main bang, by default 3000
+    min_plateau_len : int, optional
+        minimum length for which to consider a plateau in the main bang, by default 500
+    min_raw_data_len : int, optional
+        minimum possible length of the raw ultrasound data to verify if the data is valid, by default 10000
+
+    Returns
+    -------
+    int
+        index of the end of the main bang
+    """
+
+    # Value pairs of experimental values to convert from PulseCount -> approx. of bang end
+    # Tuple(PulseCount, Approximation of bang end)
+    pulse_count_to_index = [
+        (5, 1100),
+        (10, 1300),
+        (20, 1500),
+        (31, 700)
+    ]
+
+    # Find closest pulse count from dict
+    closest_pulse_count = -1
+    min_difference = float("inf")
+    for pc, i in pulse_count_to_index:
+        diff = abs(pulse_count - pc)
+        if diff < min_difference:
+            closest_pulse_count = pc
+
+    # Approximation of bang index from pulse count
+    bang_index = [pc[1] for pc in pulse_count_to_index if pc[0] == closest_pulse_count][0]
+
+    # Find the first minima following the approximation of bang index
+    # to find the true end of the bang
+    max_value_in_bang = max(data[:max_bang_len])
+    first_min_threshold = 0.7  # minima has to be less than this threshold to be considered as bang end
+    first_min_index = find_next_minimum_over_threshold(data, bang_index, first_min_threshold * max_value_in_bang, max_bang_len)
+
+    # If the bang end is further than the max bang len, return the max bang len
+    return min(first_min_index, max_bang_len)
+
+
+def auto_gain_detection(data, bang_end, signal_range=(0, 255), max_area_under_curve=2500, min_signal_height=0.5):
     """Analyses a raw ultrasound signal to determine if there need to be more or less gain applied
 
     Parameters
@@ -20,25 +149,24 @@ def auto_gain_detection_v2(data, data_range=(0, 255), bang_end=2500):
         0 -> Gain is OK
         -1 -> Gain needs to be a lower
     """
-    data = np.array(data)
-    max_value = np.max(data[bang_end:])
-    mean_value = np.mean(data[bang_end:])
+    max_value = max(data[bang_end:])
+    mean_value = sum(data[bang_end:]) / len(data[bang_end:])
 
     # Remove small noise lower than the mean
-    filt_data = data.copy()
+    filt_data = data
     filt_data[filt_data < mean_value] = 0
 
     # Use the normalized signal for the area under curve
     norm_data = filt_data / max_value
-    area_under_curve = np.sum(norm_data[bang_end:])
+    area_under_curve = sum(norm_data[bang_end:])
 
     # The max of the signal is too low, need more signal
-    if max_value < data_range[1] * 0.5:
+    if max_value < signal_range[1] * min_signal_height:
         return 1
 
     # The max of the signal is too high, or there is too much signal (the area
     # under the curve is too high), need less signal
-    if area_under_curve > 2500 or max_value == data_range[1]:
+    if area_under_curve > max_area_under_curve or max_value == signal_range[1]:
         return -1
 
     # Signal is ok: 1. max of the signal is ok, and
@@ -46,62 +174,18 @@ def auto_gain_detection_v2(data, data_range=(0, 255), bang_end=2500):
     return 0
 
 
-def auto_gain_detection(raw_signal, range=(0, 255)):
-    """Analyses a raw ultrasound signal to determine if there need to be more or less gain applied
-
-    Parameters
-    ----------
-    raw_signal : list[int/float]
-        Raw ultrasound signal
-    range : tuple, optional
-        Max and min values of the signal, by default (0, 255)
-
-    Returns
-    -------
-    int
-        2 -> Gain needs to be a lot stronger
-        1 -> Gain needs to be a little stronger
-        0 -> Gain is OK
-        -1 -> Gain needs to be a little lower
-        -2 -> Gain needs to be a lot lower
-    """
-    max_value = max(raw_signal[3000:])   # remove bang
-    if max_value < 0.4 * range[1]:
-        # Signal is very low, we need to put a lot more gain
-        return 2
-    elif max_value < 0.8 * range[1]:
-        # Signal is low, we need a little more gain
-        return 1
-    else:
-        is_clipped = [1 if d > range[1] - 1 else 0 for d in raw_signal]
-        nb_clipped_data = sum(is_clipped)
-        clipped_percentage = (nb_clipped_data / len(raw_signal)) * 100
-        print(clipped_percentage)
-
-        if clipped_percentage > 8:  # empirical "magic value"
-            # Signal is over saturated, we need to lower gain by a lot
-            return -2
-        elif clipped_percentage > 2:  # empirical "magic value"
-            # Signal is saturated, we need to lower gain
-            return -1
-
-        # Signal is OK
-        return 0
-
-
 if __name__ == "__main__":
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    import glob
+    import numpy as np
+    signal = np.genfromtxt('data/test/auc_tests/test_input_pulsecount_20.csv', delimiter=',', skip_header=1)
 
-    files = glob.glob("data/test/auc_tests/*.csv")
-    for i, f in enumerate(files, start=1):
-        plt.subplot(len(files), 1, i)
-        data = pd.read_csv(f)
-        raw = data["raw_data"].values
-        a, b, filt = auto_gain_detection_v2(raw)
-        print(i, a, b)
-        plt.plot(filt, '.')
+    bang_end = detect_main_bang_end(signal, 20)
+    auto_gain = auto_gain_detection(signal, bang_end, signal_range=(0, 255))
+
+    import matplotlib.pyplot as plt
+    plt.plot(signal)
+    plt.axvline(bang_end)
+    plt.axvline(1778, color="red")
     plt.show()
 
-    print(auto_gain_detection(raw))
+    print(f"End of main bang -> {bang_end:5d}")
+    print(f"Auto gain value  -> {auto_gain:5d}")
